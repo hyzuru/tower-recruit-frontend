a448136ecb89ad16befedc4348baffe00967ceac
Jacqueline Yeung <asatenshi@gmail.com>
2023-07-03 11:22:03 +1200

    add menu function and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import Navigation from './components/Navigation';
+import RegisterCard from './components/RegisterCard';
 import './App.css';
 
+type User = {
+  firstName: string
+  lastName?: string
+  getFullName: () => string,
+}
+
 function App() {
+
+  const user: User = { 
+    firstName: "Jacqueline",
+    lastName: "Yeung",
+    getFullName() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+
   return (
     <div className="App">
-      <Navigation/>
+      <header className="navbar">
+        <h1 className="pagetitle">Register Card Form</h1>
+        <Navigation/>
+
+      </header>
+
+      <div className="welcome">
+        Welcome, { user.getFullName() }!
+      </div>
+
+      <RegisterCard/>
     </div>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import './navigation.css'
 
 export default function Navigation() {
+
+  const [showMenu, setShowMenu] = useState(false)
+
+  let menu 
+
+  if (showMenu) {
+    menu = <div className="menu">
+        <header className="navbar">
+          <h1 className="menutitle">Menu</h1>
+          <button onClick={() => setShowMenu(false)} >
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+        </header>
+        <a href="/" className="menuitem">
+          This is a link in the menu
+        </a>
+        <a href="/" className="menuitem">
+          This is another link
+        </a>
+      </div>
+  }
+
   return (
-    <div>Navigation</div>
+    <>
+      <nav>
+        <button onClick={() => setShowMenu(!showMenu)} >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </nav>
+      {menu}
+    </>
   )
 }
